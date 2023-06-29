fcb17801a0c059f621e868461a8175e1ae8bb9f7
Wendy Miller <weendymilleer@gmail.com>
2023-06-29 09:03:58 -0300

    esboço do método que consumirá a api do serviço do bi

--- application/services/bi.py
+++ application/services/bi.py
@@ -1,4 +1,7 @@
+import requests
+
 from application.services.base import BaseService
+
 
 class BIService(BaseService):
 
@@ -10,3 +13,16 @@ class BIService(BaseService):
 
     def port(self) -> int:
         return 80
+
+    def url_env_var(self) -> str:
+     return "BI_SERVICE_URL"
+
+    def atualizarDashTwitter(self, ids: dict):
+        
+        url: str = f"{self.base_url}/atualizarDashTwitter"
+
+        response: requests.Response = requests.get(url, json=ids)
+        response.raise_for_status()
+
+        return response.json()
+
