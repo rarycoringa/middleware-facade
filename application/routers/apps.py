270ba118b8da1408d86bf1bf6cb3aaa846e374fe
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2023-06-29 04:23:07 -0300

    remove unused imports

--- application/routers/apps.py
+++ application/routers/apps.py
@@ -1,13 +1,4 @@
-import requests
-
-from typing import List
-from typing import Optional
-
 from fastapi import APIRouter
-from fastapi import HTTPException
-
-from application.services.account import AccountService
-from application.models.users import User
 
 apps_tag: str = "Apps"
 
