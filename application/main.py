aa50114683ecd07b799b65c30c1379d68cdb6454
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2023-07-01 22:27:53 -0300

    Implement BI routers, models and service (#2)
    * esboço do método que consumirá a api do serviço do bi
    
    * serviço de bi definido
    
    * code improvement updates
    
    ---------
    
    Co-authored-by: Wendy Miller <weendymilleer@gmail.com>

--- application/main.py
+++ application/main.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
 from fastapi import FastAPI
+from application.routers.bi import router as bi_router
 
 app = FastAPI()
 
@@ -9,3 +10,5 @@ async def main():
     response: Dict[str, str] = {"name": "Facade", "version": "v1.0"}
 
     return response
+
+app.include_router(bi_router)
