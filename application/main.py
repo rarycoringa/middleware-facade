e34504054201a0403d46f81e6fe8cdaa857a67cb
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2023-07-02 02:51:52 -0300

    update main file

--- application/main.py
+++ application/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from application.routers.bi import router as bi_router
 
 from application.routers.users import router as users_router
 from application.routers.users import tag as users_tag
@@ -7,6 +6,12 @@ from application.routers.users import description as users_description
 from application.routers.apps import router as apps_router
 from application.routers.apps import tag as apps_tag
 from application.routers.apps import description as apps_description
+# from application.routers.publications import router as publications_router
+# from application.routers.publications import tag as publications_tag
+# from application.routers.publications import description as publications_description
+from application.routers.bi import router as bi_router
+from application.routers.bi import tag as bi_tag
+from application.routers.bi import description as bi_description
 
 description: str = """
 **Facade API helps you to manage your social media publications** 🌐
@@ -36,6 +41,8 @@ app = FastAPI(
     openapi_tags=[
         {"name": users_tag, "description": users_description},
         {"name": apps_tag, "description": apps_description},
+        # {"name": publications_tag, "description": publications_description},
+        {"name": bi_tag, "description": bi_description},
     ],
     docs_url="/swaggerdocs",
     redoc_url="/docs",
@@ -43,3 +50,5 @@ app = FastAPI(
 
 app.include_router(users_router)
 app.include_router(apps_router)
+# app.include_router(publications_router)
+app.include_router(bi_router)
