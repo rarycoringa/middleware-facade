331740726ee70c9dbbf484bb135b698083bfad4a
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2023-07-02 01:35:32 -0300

    update openapi metadata and include routers

--- application/main.py
+++ application/main.py
@@ -1,37 +1,43 @@
 from fastapi import FastAPI
 
-# from application.routers.apps import router as apps_router
-# from application.routers.bi import router as bi_router
-# from application.routers.publications import router as publications_router
 from application.routers.users import router as users_router
+from application.routers.users import tag as users_tag
+from application.routers.users import description as users_description
+from application.routers.apps import router as apps_router
+from application.routers.apps import tag as apps_tag
+from application.routers.apps import description as apps_description
 
 description: str = """
-Facade API helps you to manage your **Social Media Content** 🌐
+**Facade API helps you to manage your social media publications** 🌐
 
-Here you will be able to enjoy all available resources on the **Social Media Middleware**, as you can see below:
-1. You can manage publications in a lot of social media plataforms at the same time.
-2. You can retrieve BI analytics about your publications and your social media accounts.
+---
+
+Here you will be able to know how to use and enjoy all available resources on the
+**Social Media Middleware**, as you can see below:
+
+- You can manage publications in a lot of social media platforms at the same time.
+- You can retrieve BI analytics about your publications and your social media accounts.
+
+---
+
+Contributors:
+- [Rary Coringa](https://github.com/rarycoringa)
+- [Wendy Miller](https://github.com/leightonisrael)
+- [Israel Hall](https://github.com/wendymillerr)
+
 """
 
 app = FastAPI(
-    title="Facade API",
+    title="Middleware Facade API",
     description=description,
-    version="1.0",
+    version="v1",
     openapi_tags=[
-        # {"name": apps_router.tags[0], "description": "some desc."},
-        # {"name": bi_router.tags[0], "description": "some desc."},
-        # {"name": publications_router.tags[0], "description": "some desc."},
-        {"name": users_router.tags[0], "description": "some desc."},
+        {"name": users_tag, "description": users_description},
+        {"name": apps_tag, "description": apps_description},
     ],
     docs_url="/swaggerdocs",
     redoc_url="/docs",
-    contact={
-        "name": "Rary Coringa",
-        "email": "rary.goncalves.123@ufrn.edu.br",
-    }
 )
 
-# app.include_router(apps_router)
-# app.include_routers(bi_router)
-# app.include_routers(publications_router)
 app.include_router(users_router)
+app.include_router(apps_router)
